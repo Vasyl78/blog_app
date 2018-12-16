28f8e6e88426ad8540b4b6f24f4ca0ec3feb093e
Vasyl78 <terekh.vasyl@gmail.com>
2018-12-17 00:44:08 +0200

    Build seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,31 @@
 # frozen_string_literal: true
 
-# Seeds will be here
+# Create categories with posts and comments
+10.times do
+  category = Category.create(
+    name: 'Category numb.',
+    description: Faker::BackToTheFuture.quote
+  )
+
+  Comment.create(
+    author: "#{Faker::Name.name}.",
+    content: Faker::BackToTheFuture.quote,
+    commentable: category
+  )
+  20.times do
+    content = Array.new(10).map do |_el|
+      Faker::Lorem.paragraph
+    end.join("\n")
+    post = Post.create(
+      name: 'Post number.',
+      content: content,
+      category: category
+    )
+
+    Comment.create(
+      author: "#{Faker::Name.name}.",
+      content: Faker::BackToTheFuture.quote,
+      commentable: post
+    )
+  end
+end
